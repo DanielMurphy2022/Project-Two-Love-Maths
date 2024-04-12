e5133b5a975bdf9aff868fdd05481640ae3fcb9d
Daniel Murphhy <danomurphy@live.ie>
2024-04-12 09:04:30 +0000

    Changed some CSS, added final comments over. Everyting is transfered now

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -1,10 +1,13 @@
+/* Users starting score 0,0 */
 let playerScore = 0;
 let computerScore = 0;
 
+/* adding math.random for random selection from the choices array */
 function play(playerChoice) {
   const choices = ['rock', 'paper', 'scissors'];
   const computerChoice = choices[Math.floor(Math.random() * 3)];
-
+  
+  /* JS for the results outcome, if,else if and else statements */
   let result;
   if (playerChoice === computerChoice) {
     result = "It's a tie!";
@@ -19,7 +22,8 @@ function play(playerChoice) {
     result = 'Computer wins!';
     computerScore++;
   }
-
+  /* Updates the webpage for user to see the score of latest round */
   document.getElementById('result').innerText = result;
+  /* Show current score, both user and computer score */
   document.getElementById('score').innerText = `Score: Player - ${playerScore}, Computer - ${computerScore}`;
 }
